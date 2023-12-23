7372c526dbda8a7ca389df9e85e56bd7c322ed5f
Yovan Julio <yovanjulioadam@gmail.com>
2023-12-23 17:35:52 +0700

    added iqtest videos

--- mixins/siteDatas.ts
+++ mixins/siteDatas.ts
@@ -69,8 +69,8 @@ const pageDatas: pageDataType = {
       ],
       yearStarted: 2022,
       projectName: "SMShub",
-      tags: ["HTML", "Bootstrap 5", "JavaScript"],
-      category: CategoryEnum.FE,
+      tags: ["PHP", "Bootstrap 5", "JavaScript"],
+      category: CategoryEnum.FS,
       link: "https://smshub360.com",
       classes: ["pt-0", "md:col-span-7", "md:pt-10"],
       imgAltName: "SMShub landing page mockup",
@@ -91,7 +91,7 @@ const pageDatas: pageDataType = {
       tags: ["HTML", "Bootstrap 5", "JavaScript"],
       category: CategoryEnum.FE,
       link: "https://xlwinmedia.com",
-      classes: ["pt-0", "md:col-span-5", "md:pt-32"],
+      classes: ["pt-0", "md:col-span-5", "md:pt-20"],
       imgAltName: "XLWIN MEDIA landing page mockup",
     },
     {
@@ -116,18 +116,18 @@ const pageDatas: pageDataType = {
     {
       videos: [
         {
-          link: "https://res.cloudinary.com/djsa72jl/video/upload/t_portfolio/v1703284304/cuppatea_promo_ocmumt.webm",
+          link: "https://res.cloudinary.com/djsa72jl/video/upload/c_fit,h_281,w_500/v1703327088/my-iqtest_promo_sfnns0.webm",
           type: "video/webm",
         },
         {
-          link: "https://res.cloudinary.com/djsa72jl/video/upload/t_portfolio/v1703284304/cuppatea_promo_ocmumt.mp4",
+          link: "https://res.cloudinary.com/djsa72jl/video/upload/c_fit,h_281,w_500/v1703327088/my-iqtest_promo_sfnns0.mp4",
           type: "video/mp4",
         },
       ],
       yearStarted: 2023,
       projectName: "My-IqTest",
-      tags: ["HTML", "Bootstrap 5", "JavaScript"],
-      category: CategoryEnum.FE,
+      tags: ["PHP", "Bootstrap 5", "JavaScript"],
+      category: CategoryEnum.FS,
       link: "https://my-iqtest.com",
       classes: ["h-fit", "md:col-span-4"],
       imgAltName: "MY-IQTEST landing page mockup",
